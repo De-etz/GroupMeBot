140be8681e04f5cfdd1a5e1609abc029231145a9
De-etz <adityakumarsingh2100@gmail.com>
2017-08-23 18:43:40 -0400

    Auto-update

--- bot.js
+++ bot.js
@@ -246,25 +246,25 @@ function announce(round) {
 	};
 	
 	var idsSmall = new Array();
-	int k = 0;
+	var k = 0;
 	for (i = (round-1)*maxUsers; i < round*maxUsers; i++) {
 		if (i > ids.length) {
 			break;
 		}
-		// idsSmall[k] = ids[i];
-		// k++;
+		idsSmall[k] = ids[i];
+		k++;
 	}
 	
 	postMessage(idsSmall.toString());
 
 	var loci = new Array();
-	// k = 0;
+	k = 0;
 	for (i = 0; i < maxUsers; i++) {
 		if (i > ids.length) {
 			break;
 		}
-		// loci[k] = [0,0];
-		// k++;
+		loci[k] = [0,0];
+		k++;
 	}
 	
 	postMessage(loci.toString());
