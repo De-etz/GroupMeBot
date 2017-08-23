2dae846fd8b9c1e4c205a80775d93a683270b59d
De-etz <adityakumarsingh2100@gmail.com>
2017-08-23 16:31:33 -0400

    Auto-update

--- bot.js
+++ bot.js
@@ -10,21 +10,66 @@ var intro = 0;
 
 //User IDs
 var bot = 		395976;
-var aditya =	28705961;
-var pranav =	25448183;
-var rohith =	43484221;
-var joey =		29263943;
-var zach =		26215931;
-var tim =		47476114;
-var ian =		26281232;
-var ken =		29454584;
-var alice =		7668435;
-var claire =	7668436;
-var kelley =	24220651;
-var tori =		28690781;
+var adityas =	28705961;
+var alainas =	26324111;
+var alexa =		37554485;
+var alexb =		28551558;
+var alexisk		43694487;
+var alicem =	7668435;
+var alicet =	43030267;
+var annalig =	34197001;
+var anorai =	23240013;
+var blairc =	25448184;
+var brycew =	31890188;
+var camdens =	31072505;
+var catheriner =24098720;
+var chasec =	27945968;
+var claireg =	7668436;
+var dzidupeek =	37853175;
+var elizabethh =5238889;
+var emmaw =		30154745;
+var evanm =		31655413;
+var harir =		20639763;
+var gabbic =	32320507;
+var hunterc =	8830642;
+var ianc =		26281232;
+var isabelj =	24401015;
+var jackr =		26215932;
+var jareda =	26233424;
+var jbc =		28181092;
+var jenniez =	34278340;
+var joeyt =		29263943;
+var joshw =		8931279;
 var juliag =	33611553;
-var isabel =	24401015;
-var ids = [bot, aditya, pranav, rohith, joey, zach, tim, ian, ken, alice, claire, kelley, tori, juliag, isabel];
+var juliap =	28200787;
+var justinp =	34562803;
+var kaneb =		31266486;
+var kelleyl =	24220651;
+var kenp =		29454584;
+var laurenl =	7668434;
+var laurens =	33335699;
+var lornaf =	32860874;
+var madisonk =	5238905;
+var makennar =	37631615;
+var michaelc =	8830643;
+var nicks =		26215934;
+var nickw =		38803444;
+var noahh =		24037063;
+var oliviaw =	8770582;
+var palinah =	31652199;
+var pranavr =	25448183;
+var rahulc =	35866129;
+var rohithp =	43484221;
+var shraddhap =	30155431;
+var simmid =	9603427;
+var simons =	27319066;
+var sophiet =	32633725;
+var spencerg =	48026577;
+var timk =		47476114;
+var toric =		28690781;
+var zachd =		26215931;
+
+var ids = [bot, adityaa, alainas, alexa, alexb, alexisk, alicem, alicet, annalig, anorai, blairc, brycew, camdens, catheriner, chasec, claireg, dzidupeek, elizabethh, emmaw, evanm, gabbic, harir, hunterc, ianc, isabelj, jackr, jareda, jbc, jenniez, joeyt, joshw, juliag, juliap, justinp, kaneb, kelleyl, kenp, laurenl, laurens, lornaf, madisonk, makennar, michaelc, nicks, nickw, noahh, oliviaw, palinah, pranavr, rahulc, rohithp, shraddhap, simmid, simons, sophiet, spencerg, timk, toric, zachd];
 
 //Commands
 var command = '/',
@@ -90,7 +135,7 @@ function processCommand(request) {
 	} else if (is(request, unlock)) {
 		//Silent ignore
 	} else if (is(request, summon)) {
-		summonUser('Summoned.');
+		summonUser('Avengers, assemble!');
 	} else {
 		postMessage('Unknown command. Use "/help" for a list of commands'); 
 	}
@@ -182,6 +227,49 @@ function searchGiphy(giphyToSearch) {
 	};
 
 	HTTP.request(options, callback).end();
+}
+
+function announce(message) {
+	var botResponse, options, body, botReq;
+
+	botResponse = message;
+	
+	options = {
+		hostname: 'api.groupme.com',
+		path: '/v3/bots/post',
+		method: 'POST'
+	};
+	
+	var loci = new Array(ids.length);
+	loci.fill([0,0]);
+	
+	body = {
+		"bot_id" : botID,
+		"text" : botResponse,
+		"attachments" : [{
+			"type" : "mentions",
+			"user_ids" : ids,
+			"loci" : loci
+		}]
+	};
+
+	console.log('sending ' + botResponse + ' to ' + botID);
+
+	botReq = HTTPS.request(options, function(res) {
+			if(res.statusCode == 202) {
+				//neat
+			} else {
+				console.log('rejecting bad status code ' + res.statusCode);
+			}
+	});
+
+	botReq.on('error', function(err) {
+		console.log('error posting message '	+ JSON.stringify(err));
+	});
+	botReq.on('timeout', function(err) {
+		console.log('timeout posting message '	+ JSON.stringify(err));
+	});
+	botReq.end(JSON.stringify(body));
 }
 
 function summonUser(message) {
