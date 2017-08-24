0d85ee5fd91e8f7501e30fcb71005b34b97f5627
De-etz <adityakumarsingh2100@gmail.com>
2017-08-23 20:56:16 -0400

    Auto-update

--- bot.js
+++ bot.js
@@ -6,25 +6,70 @@ var botID = process.env.BOT_ID;
 var apiKey = process.env.API_KEY;
 var locked = false;
 
-var intro = 1;
+var intro = 0;
 
-//User IDs
+// User IDs
 var bot = 		395976;
-var me = 		28705961;
-var pranav = 	25448183;
-var rohith = 	43484221;
-var joey = 		29263943;
-var zach = 		26215931;
-var tim = 		47476114;
-var ian = 		26281232;
-var ken = 		29454584;
-var alice =		7668435;
-var claire =	7668436;
-var kelley =	24220651;
-var tori =		28690781;
+var adityas =	28705961;
+var alainas =	26324111;
+var alexa =		37554485;
+var alexb =		28551558;
+var alexisk =	43694487;
+var alicem =	7668435;
+var alicet =	43030267;
+var annalig =	34197001;
+var anorai =	23240013;
+var blairc =	25448184;
+var brycew =	31890188;
+var camdens =	31072505;
+var catheriner =24098720;
+var chasec =	27945968;
+var claireg =	7668436;
+var dzidupeek =	37853175;
+var elizabethh =5238889;
+var emmaw =		30154745;
+var evanm =		31655413;
+var harir =		20639763;
+var gabbic =	32320507;
+var hunterc =	8830642;
+var ianc =		26281232;
+var isabelj =	24401015;
+var jackr =		26215932;
+var jareda =	26233424;
+var jbc =		28181092;
+var jenniez =	34278340;
+var joeyt =		29263943;
+var joshw =		8931279;
 var juliag =	33611553;
-var isabel =	24401015;
-var ids = [bot, me, pranav, rohith, joey, zach, tim, ian, ken, alice, claire, kelley, tori, juliag, isabel];
+var juliap =	28200787;
+var justinp =	34562803;
+var kaneb =		31266486;
+var kelleyl =	24220651;
+var kenp =		29454584;
+var laurenl =	7668434;
+var laurens =	33335699;
+var lornaf =	32860874;
+var madisonk =	5238905;
+var makennar =	37631615;
+var michaelc =	8830643;
+var nicks =		26215934;
+var nickw =		38803444;
+var noahh =		24037063;
+var oliviaw =	8770582;
+var palinah =	31652199;
+var pranavr =	25448183;
+var rahulc =	35866129;
+var rohithp =	43484221;
+var shraddhap =	30155431;
+var simmid =	9603427;
+var simons =	27319066;
+var sophiet =	32633725;
+var spencerg =	48026577;
+var timk =		47476114;
+var toric =		28690781;
+var zachd =		26215931;
+
+var ids = [bot, adityas, alainas, alexa, alexb, alexisk, alicem, alicet, annalig, anorai, blairc, brycew, camdens, catheriner, chasec, claireg, dzidupeek, elizabethh, emmaw, evanm, gabbic, harir, hunterc, ianc, isabelj, jackr, jareda, jbc, jenniez, joeyt, joshw, juliag, juliap, justinp, kaneb, kelleyl, kenp, laurenl, laurens, lornaf, madisonk, makennar, michaelc, nicks, nickw, noahh, oliviaw, palinah, pranavr, rahulc, rohithp, shraddhap, simmid, simons, sophiet, spencerg, timk, toric, zachd];
 
 //Commands
 var command = '/',
@@ -33,8 +78,9 @@ var command = '/',
 	face = '/coolGuy',
 	help = '/help',
 	info = '/displayInfo',
-	gif = '/gif';
-var commands = [command, lock, unlock, face, help, info, gif];
+	gif = '/gif',
+	summon = '/summon';
+var commands = [command, lock, unlock, face, help, info, gif, summon];
 
 function listCommands(request) {
 	var cList = '';
@@ -50,7 +96,7 @@ function listCommands(request) {
 	} else {
 		cList += 'List of user commands (For all commands, use "/help all")\r\n';
 		for (i = 1; i < commands.length; i++) {
-			if (commands[i] === lock || commands[i] === unlock) {
+			if (commands[i] === lock || commands[i] === unlock || commands[i] === summon) {
 				
 			} else if (commands[i] === gif) {
 				cList += commands[i] + " [search query] (Use responsibly...)\r\n";
@@ -71,9 +117,7 @@ function is(request, command) {
 }
 	
 function processCommand(request) {
-	if (ids.indexOf(parseInt(request.user_id)) === -1) {
-		postMessage(parseInt(request.user_id) + ' (Ignore this)');
-	}
+	
 	if (is(request, face)) {
 		postMessage(cool());
 	} else if (is(request, help)) {
@@ -81,13 +125,17 @@ function processCommand(request) {
 	} else if (is(request, info)) {
 		displayInfo(request);
 	} else if (is(request, gif)) {
-		if (request.name == 'Jb Core') {
+		if (parseInt(request.user_id) == jbc) {
 			postMessage('No.');
 		} else {
 			searchGiphy(request.text.substring(gif.length + 1));
 		}
 	} else if (is(request, unlock)) {
 		//Silent ignore
+	} else if (is(request, summon)) {
+		if (parseInt(request.user_id) == adityas) {
+			summonAll();
+		}
 	} else {
 		postMessage('Unknown command. Use "/help" for a list of commands'); 
 	}
@@ -104,10 +152,10 @@ function respond() {
 		if (locked) return;
 		
 		//Keep Pranav in line
-		if (request.text && parseInt(request.user_id) === pranav) { //Pranav response
+		if (request.text && parseInt(request.user_id) === pranavr) { //Pranav response
 			var rand = Math.floor((Math.random() * 100) + 1);
 			postMessage(rand);
-			if (rand <= 10) {postMessage("Fuck off Pranav.");}
+			if (rand <= 33) {postMessage("Fuck off Pranav.");}
 		}
 		
 		//Run command
@@ -133,7 +181,7 @@ function manageLock(key) {
 	//Check if locking, unlocking, or neither
 	if (key.text && key.text.substring(0, lock.length) == lock) {
 		
-		if (parseInt(key.user_id) === me) {
+		if (parseInt(key.user_id) === adityas) {
 			if (locked) {
 				postMessage("Already locked.");
 			} else {
@@ -142,7 +190,7 @@ function manageLock(key) {
 			locked = true;
 		}
 	} else if (key.text && key.text.substring(0, unlock.length) == unlock) {
-		if (parseInt(key.user_id) === me) {
+		if (parseInt(key.user_id) === adityas) {
 			if (!locked) {
 				postMessage("Already unlocked.");
 			} else {
@@ -180,6 +228,74 @@ function searchGiphy(giphyToSearch) {
 
 	HTTP.request(options, callback).end();
 }
+
+function summonAll() {
+	
+	var userCount = ids.length;
+	var midpoint = Math.round(userCount/2);
+	
+	var firstHalf = [];
+	var k = 0;
+	for (i = 0; i < midpoint; i++) {
+		firstHalf[k] = ids[i];
+		k++;
+	}
+	summonUsers(firstHalf);
+	
+	var secondHalf = [];
+	k = 0;
+	for (i = midpoint; i < userCount; i++) {
+		secondHalf[k] = ids[i];
+		k++
+	}
+	summonUsers(secondHalf);
+}
+
+function summonUsers(users) {
+	var botResponse, options, body, botReq;
+	
+	botResponse = "Like these messages if you received a notification from me (the bot)";
+	
+	options = {
+		hostname: 'api.groupme.com',
+		path: '/v3/bots/post',
+		method: 'POST'
+	};
+
+	var loci = new Array();
+	for (i = 0; i < users.length; i++) {
+		loci[i] = [0,19];
+	}
+	
+	body = {
+		"bot_id" : botID,
+		"text" : botResponse,
+		"attachments" : [{
+			"type" : "mentions",
+			"user_ids" : users,
+			"loci" : loci
+		}]
+	};
+
+	console.log('sending ' + botResponse + ' to ' + botID);
+
+	botReq = HTTPS.request(options, function(res) {
+			if(res.statusCode == 202) {
+				//neat
+			} else {
+				console.log('rejecting bad status code ' + res.statusCode);
+			}
+	});
+
+	botReq.on('error', function(err) {
+		console.log('error posting message '	+ JSON.stringify(err));
+	});
+	botReq.on('timeout', function(err) {
+		console.log('timeout posting message '	+ JSON.stringify(err));
+	});
+	botReq.end(JSON.stringify(body));
+}
+
 
 function postMessage(message) {
 	var botResponse, options, body, botReq;
